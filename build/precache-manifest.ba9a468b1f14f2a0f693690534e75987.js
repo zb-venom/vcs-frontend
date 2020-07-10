44716446bd2231c24d24f57bd00361eb094bbf68
ZB Tyoma <zb.tyoma@gmail.com>
2020-07-10 16:58:49 +0700

    master

---
+++ build/precache-manifest.ba9a468b1f14f2a0f693690534e75987.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3be10d7705a079cbb1fe9e0290657a",
+    "url": "/vcs-frontend/index.html"
+  },
+  {
+    "revision": "2ad6d08149152e1d4fa6",
+    "url": "/vcs-frontend/static/js/main.a89d09b6.chunk.js"
+  },
+  {
+    "revision": "47009ce426034c9bc318",
+    "url": "/vcs-frontend/static/js/runtime-main.a356d3ba.js"
+  }
+]);
---
+++ build/static/js/main.a89d09b6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvcs-frontend"]=this["webpackJsonpvcs-frontend"]||[]).push([[0],[function(n,s,o){n.exports=o(1)},function(n,s){}],[[0,1]]]);
+//# sourceMappingURL=main.a89d09b6.chunk.js.map
